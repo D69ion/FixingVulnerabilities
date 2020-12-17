e5ba70180094d7a5e71c6da8ae92c6757243cca0
D69ion <dmitriy.shtele@gmail.com>
2020-12-17 19:30:26 +0400

    Добавьте файлы проекта.

---
+++ FixingVulnerabilities/WithVulnerabilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixingVulnerabilities
+{
+    class WithVulnerabilities
+    {
+        public void Execute(string data)
+        {
+            Logger logger;
+            //вставка данных как есть, содержащих уязвимость
+            logger.Warn(data + " log in requested"); 
+        }
+    }
+}
---
+++ FixingVulnerabilities/WithoutVulnerabilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixingVulnerabilities
+{
+    class WithoutVulnerabilities
+    {
+        public void Execute(string data)
+        {
+            Logger logger;
+            //вставка измененных данных, которые избавлены от уязвимости
+            logger.Warn(data.Replace(Environment.NewLine, "") + " log in requested");
+        }
+    }
+}
